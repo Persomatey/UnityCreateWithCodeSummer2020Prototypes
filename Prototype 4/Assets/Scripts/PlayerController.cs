dfda07580c31b49f6a4b8ec5f355d61a71dd5b49
Persomatey <huntergoodin1@hotmail.com>
2020-08-23 15:38:58 -0700

    Added Prototype 4

---
+++ Prototype 4/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float speed;
+    private GameObject focalPoint; 
+
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("FocalPoint"); 
+    }
+
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+    }
+}
