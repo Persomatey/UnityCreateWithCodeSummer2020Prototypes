7b3506fe649ede2523e833bb144a86bbd99f0476
Hunter Goodin <huntergoodin1@hotmail.com>
2020-08-25 23:45:34 -0700

    Made Changes to Prototype 4

---
+++ Prototype 4/Assets/Challenge 4/Scripts/FollowPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    private GameObject playerObj;
+
+    void Start()
+    {
+        playerObj = GameObject.Find("Player");
+    }
+
+    void Update()
+    {
+        transform.position = playerObj.transform.position;
+        transform.rotation = new Quaternion(transform.rotation.x, playerObj.transform.rotation.y - 180, transform.rotation.z, transform.rotation.w); 
+    }
+}
